2a2c9817364b38aa6ec08f10a8d5434629849cbd
Tran Trong Kim <kakixavo@gmail.com>
2021-08-23 00:25:45 +0700

    chore: HighCommentWidget

--- lib/high_comment.dart
+++ lib/high_comment.dart
@@ -4,10 +4,21 @@ import 'package:flutter/rendering.dart';
 class HighComment extends RenderObjectWidget {
   final Widget avatar;
   final Widget comment;
+  final Widget actions;
+  final Widget reaction;
+
+  final bool shouldDrawTopLine;
+  final bool shouldDrawBottomLine;
+  final bool isParentComment;
 
   HighComment({
     this.avatar = const SizedBox(),
     this.comment = const SizedBox(),
+    this.actions = const SizedBox(),
+    this.reaction = const SizedBox(),
+    this.shouldDrawBottomLine = false,
+    this.shouldDrawTopLine = false,
+    this.isParentComment = true,
   });
 
   @override
@@ -17,13 +28,34 @@ class HighComment extends RenderObjectWidget {
 
   @override
   RenderObject createRenderObject(BuildContext context) {
-    return HighCommentBox();
+    context as _HighCommentElement;
+    return HighCommentBox()
+      ..isParentComment = isParentComment
+      ..shouldDrawBottomLine = shouldDrawBottomLine
+      ..shouldDrawTopLine = shouldDrawTopLine;
   }
+
+  @override
+  void updateRenderObject(BuildContext context, HighCommentBox renderObject) {
+    renderObject
+      ..isParentComment = isParentComment
+      ..shouldDrawTopLine = shouldDrawTopLine
+      ..shouldDrawBottomLine = shouldDrawBottomLine;
+  }
+}
+
+enum _CommentIdentify {
+  avatar,
+  comment,
+  actions,
+  reaction,
 }
 
 class _HighCommentElement extends RenderObjectElement {
   Element? _avatar;
   Element? _comment;
+  Element? _actions;
+  Element? _reaction;
   _HighCommentElement(HighComment widget) : super(widget);
 
   @override
@@ -34,16 +66,23 @@ class _HighCommentElement extends RenderObjectElement {
   @override
   void mount(Element? parent, Object? newSlot) {
     super.mount(parent, newSlot);
-    _avatar = inflateWidget(widget.avatar, true);
-    _comment = inflateWidget(widget.comment, true);
+    _avatar = inflateWidget(widget.avatar, _CommentIdentify.avatar);
+    _comment = inflateWidget(widget.comment, _CommentIdentify.comment);
+    _actions = inflateWidget(widget.actions, _CommentIdentify.actions);
+    _reaction = inflateWidget(widget.reaction, _CommentIdentify.reaction);
   }
 
   @override
   void update(covariant HighComment newWidget) {
     super.update(newWidget);
 
-    _avatar = updateChild(_avatar, newWidget.avatar, true);
-    _comment = updateChild(_comment, newWidget.comment, true);
+    _avatar = updateChild(_avatar, newWidget.avatar, _CommentIdentify.avatar);
+    _comment =
+        updateChild(_comment, newWidget.comment, _CommentIdentify.comment);
+    _actions =
+        updateChild(_actions, newWidget.actions, _CommentIdentify.actions);
+    _reaction =
+        updateChild(_reaction, newWidget.reaction, _CommentIdentify.reaction);
   }
 
   @override
@@ -51,12 +90,16 @@ class _HighCommentElement extends RenderObjectElement {
     super.unmount();
     _avatar = null;
     _comment = null;
+    _actions = null;
+    _reaction = null;
   }
 
   @override
   void visitChildren(ElementVisitor visitor) {
     if (_avatar != null) visitor(_avatar!);
     if (_comment != null) visitor(_comment!);
+    if (_actions != null) visitor(_actions!);
+    if (_reaction != null) visitor(_reaction!);
   }
 
   @override
@@ -64,27 +107,59 @@ class _HighCommentElement extends RenderObjectElement {
     super.forgetChild(child);
     if (_avatar == child) _avatar = null;
     if (_comment == child) _comment = null;
+    if (_actions == child) _actions = null;
+    if (_reaction == child) _reaction = null;
   }
 
   @override
-  void insertRenderObjectChild(RenderBox child, bool slot) {
-    renderObject.inseartRenderObjectChild(child, slot);
+  void insertRenderObjectChild(RenderBox child, covariant Object? slot) {
+    renderObject.insertRenderObjectChild(child, slot as _CommentIdentify);
   }
 
   @override
-  void moveRenderObjectChild(RenderBox child, bool oldSlot, bool newSlot) {
-    renderObject.moveRenderObjectChild(child, oldSlot, newSlot);
+  void moveRenderObjectChild(
+      RenderBox child, _CommentIdentify? oldSlot, _CommentIdentify? newSlot) {
+    renderObject.moveRenderObjectChild(child, oldSlot!, newSlot!);
   }
 
   @override
-  void removeRenderObjectChild(RenderBox child, bool slot) {
-    renderObject.removeRenderObjectChild(child, slot);
+  void removeRenderObjectChild(RenderBox child, _CommentIdentify? slot) {
+    renderObject.removeRenderObjectChild(child, slot!);
   }
 }
 
 class HighCommentBox extends RenderBox {
   RenderBox? _avatar;
   RenderBox? _comment;
+  RenderBox? _actions;
+  RenderBox? _reaction;
+
+  bool _shouldDrawTopLine = false;
+
+  bool get shouldDrawTopLine => _shouldDrawTopLine;
+
+  set shouldDrawTopLine(bool shouldDrawTopLine) {
+    _shouldDrawTopLine = shouldDrawTopLine;
+    markNeedsPaint();
+  }
+
+  bool _shouldDrawBottomLine = false;
+
+  bool get shouldDrawBottomLine => _shouldDrawBottomLine;
+
+  set shouldDrawBottomLine(bool shouldDrawBottomLine) {
+    _shouldDrawBottomLine = shouldDrawBottomLine;
+    markNeedsPaint();
+  }
+
+  bool _isParentComment = true;
+
+  bool get isParentComment => _isParentComment;
+
+  set isParentComment(bool isParentComment) {
+    _isParentComment = isParentComment;
+    markNeedsPaint();
+  }
 
   @override
   void attach(covariant PipelineOwner owner) {
@@ -92,6 +167,8 @@ class HighCommentBox extends RenderBox {
 
     _avatar?.attach(owner);
     _comment?.attach(owner);
+    _actions?.attach(owner);
+    _reaction?.attach(owner);
   }
 
   @override
@@ -99,6 +176,8 @@ class HighCommentBox extends RenderBox {
     super.detach();
     _avatar?.detach();
     _comment?.detach();
+    _actions?.detach();
+    _reaction?.detach();
   }
 
   @override
@@ -106,6 +185,8 @@ class HighCommentBox extends RenderBox {
     super.visitChildren(visitor);
     if (_avatar != null) visitor(_avatar!);
     if (_comment != null) visitor(_comment!);
+    if (_actions != null) visitor(_actions!);
+    if (_reaction != null) visitor(_reaction!);
   }
 
   @override
@@ -113,98 +194,154 @@ class HighCommentBox extends RenderBox {
     super.redepthChildren();
     _avatar?.redepthChildren();
     _comment?.redepthChildren();
+    _actions?.redepthChildren();
+    _reaction?.redepthChildren();
   }
 
-  void inseartRenderObjectChild(RenderBox child, bool slot) {
-    if (slot) {
-      _avatar = child;
-    } else {
-      _comment = child;
+  void _updateChild(RenderBox? child, _CommentIdentify slot) {
+    switch (slot) {
+      case _CommentIdentify.avatar:
+        _avatar = child;
+        break;
+      case _CommentIdentify.comment:
+        _comment = child;
+        break;
+      case _CommentIdentify.actions:
+        _actions = child;
+        break;
+      case _CommentIdentify.reaction:
+        _reaction = child;
+        break;
     }
+  }
+
+  void insertRenderObjectChild(RenderBox child, _CommentIdentify slot) {
+    _updateChild(child, slot);
 
     adoptChild(child);
   }
 
-  void moveRenderObjectChild(RenderBox child, bool oldSlot, bool newSlot) {
-    if (oldSlot) {
-      _avatar = null;
-    } else {
-      _comment = null;
-    }
-    if (newSlot) {
-      _avatar = child;
-    } else {
-      _comment = child;
-    }
+  void moveRenderObjectChild(
+      RenderBox child, _CommentIdentify oldSlot, _CommentIdentify newSlot) {
+    _updateChild(null, oldSlot);
+    _updateChild(child, newSlot);
   }
 
-  void removeRenderObjectChild(RenderBox child, bool slot) {
-    if (slot) {
-      _avatar = null;
-    } else {
-      _comment = null;
-    }
+  void removeRenderObjectChild(RenderBox child, _CommentIdentify slot) {
+    _updateChild(null, slot);
 
     dropChild(child);
   }
 
   @override
   void performLayout() {
-    // var childConstraints = constraints;
-
-    // if (_avatar != null) {
-    //   _avatar!.layout(
-    //       BoxConstraints(
-    //           // maxWidth: avatar.size.width,
-    //           ),
-    //       parentUsesSize: true);
-    // }
-
-    // if (_comment != null) {
-    //   _comment!.layout(childConstraints, parentUsesSize: true);
-    // }
-
-    // if (_avatar == null && _comment == null) {
-    //   size = constraints.smallest;
-    // } else if (_avatar == null && _comment != null) {
-    //   size = _comment!.size;
-    // } else if (_comment == null) {
-    //   size = _avatar!.size;
-    // } else if (_avatar!.size.height > _comment!.size.height) {
-    //   size = _avatar!.size;
-    // } else {
-    //   size = _comment!.size;
-    // }
-
-    var followerConstraints = constraints;
-
-    final child = _avatar;
-    if (child != null) {
-      child.layout(constraints, parentUsesSize: true);
-      followerConstraints = BoxConstraints.tight(child.size);
+    double heightActionAndReaction = 0;
+    double height = 0;
+    if (_avatar != null) {
+      _avatar!.layout(
+          BoxConstraints(
+            maxWidth: 32,
+          ),
+          parentUsesSize: true);
     }
 
-    final overlay = _comment;
-    if (overlay != null) {
-      overlay.layout(followerConstraints, parentUsesSize: true);
+    if (_comment != null) {
+      _comment!.layout(
+          BoxConstraints(
+              maxWidth: constraints.maxWidth - 76 - (isParentComment ? 0 : 44)),
+          parentUsesSize: true);
+      height = _comment!.size.height;
     }
 
-    size = _avatar?.size ?? _comment?.size ?? constraints.smallest;
+    if (_actions != null) {
+      _actions!.layout(BoxConstraints(), parentUsesSize: true);
+      heightActionAndReaction = _actions!.size.height;
+    }
+    if (_reaction != null) {
+      _reaction!.layout(BoxConstraints(), parentUsesSize: true);
+      if (_reaction!.size.height > heightActionAndReaction) {
+        heightActionAndReaction = _reaction!.size.height;
+      }
+    }
+
+    size = Size(constraints.maxWidth, height + heightActionAndReaction);
   }
 
   @override
   bool get hasSize => true;
 
-  @override
-  void paint(PaintingContext context, Offset offset) {
-    // if (_comment != null) {
-    //   context.paintChild(_comment!, offset);
-    // }
+  void _paintChild(PaintingContext context, Offset offset) {
+    double translate = isParentComment ? 0 : 44;
+    Offset actionOffset = offset;
     if (_avatar != null) {
-      context.paintChild(_avatar!, offset);
+      context.paintChild(_avatar!, offset.translate(16 + translate, 18));
     }
 
+    if (_comment != null) {
+      context.paintChild(_comment!, offset.translate(60 + translate, 0));
+      actionOffset = offset.translate(60 + translate, _comment!.size.height);
+    }
+    if (_actions != null) {
+      context.paintChild(_actions!, actionOffset);
+    }
+  }
+
+  void _paintLine(Canvas canvas, Offset offset) {
+    final paint = Paint()
+      ..color = Color(0xFFE2E7ED)
+      ..strokeWidth = 1
+      ..isAntiAlias = true;
+    canvas.save();
+    if (isParentComment) {
+      if (_avatar == null && _shouldDrawTopLine && _shouldDrawBottomLine) {
+        final o = offset.translate(30, 0);
+        canvas.drawLine(o, o.translate(0, size.height), paint);
+      } else {
+        if (_shouldDrawTopLine && _avatar != null) {
+          final s = _avatar!.size;
+          canvas.drawLine(offset.translate(s.width / 2 + 16, 0),
+              offset.translate(s.width / 2 + 16, 13), paint);
+        }
+        if (_shouldDrawBottomLine && _avatar != null && _comment != null) {
+          final s = _avatar!.size;
+
+          canvas.drawLine(offset.translate(s.width / 2 + 16, s.height + 26),
+              offset.translate(s.width / 2 + 16, size.height), paint);
+        }
+      }
+    } else {
+      if (_shouldDrawTopLine || _shouldDrawBottomLine && !isParentComment) {
+        final o = offset.translate(32, 0);
+        canvas.drawLine(o, o.translate(0, size.height), paint);
+      }
+    }
+    double translate = isParentComment ? 0 : 44;
+    if (_avatar == null && _shouldDrawTopLine && _shouldDrawBottomLine) {
+      final o = offset.translate(30 + translate, 0);
+      canvas.drawLine(o, o.translate(0, size.height), paint);
+    } else {
+      if (_shouldDrawTopLine && _avatar != null) {
+        final s = _avatar!.size;
+        canvas.drawLine(offset.translate(s.width / 2 + 16 + translate, 0),
+            offset.translate(s.width / 2 + 16 + translate, 13), paint);
+      }
+      if (_shouldDrawBottomLine && _avatar != null && _comment != null) {
+        final s = _avatar!.size;
+
+        canvas.drawLine(
+            offset.translate(s.width / 2 + 16 + translate, s.height + 26),
+            offset.translate(s.width / 2 + 16 + translate, size.height),
+            paint);
+      }
+    }
+    canvas.restore();
+  }
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
     final canvas = context.canvas;
+    _paintChild(context, offset);
+    _paintLine(canvas, offset);
   }
 
   @override
--- lib/home.dart
+++ lib/home.dart
@@ -8,47 +8,61 @@ import 'package:render_object/comment.dart';
 import 'package:render_object/high_comment.dart';
 import 'package:render_object/process_bar.dart';
 
-class HomePage extends StatelessWidget {
+final comments = <String>[
+  'Nhìn nhảm ghê á trời',
+  'Ủa không xem thôi mắc gì bảo nhảm',
+  'ủa chứ xem xong, không thích không được comment à, lạ đời',
+  'Mấy ông học văn bảo: "Anh với em như hyperbole và đường tiệm cận/ Mãi gần nhau nhưng chẳng gặp nhau". Thời gian sau này có ông thêm vào: "Chỉ sợ nay mai em lỡ có bầu/ Anh không muốn cũng thôi làm đường conic".',
+  '''Thật ra khi bạn học đến phần tiệm cận và hàm phân thức bậc nhất trên bậc nhất đặt câu hỏi này là bình thường, và ý hỏi của bạn ý sâu xa hơn. Mình từng có hs hỏi câu này rồi.
+Cái này một phần là do chương trình học chỉ trình bày gói gọn một số hàm và đồ thị của nó, nên học sinh khó hình dung và mở rộng tư duy ra các hàm khác được. Lại thêm thi tno có mấy trò học mẹo nữa, nên lại càng khó hiểu bản chất.
+Ý hỏi của bạn ấy theo mình đoán là thế này, ví dụ như hàm y=1/x nhận tcn là đt x=0 thì lúc đó khi x dần đến vô cùng ta có coi là đths cắt được đt x=0 hay ko? Thì câu trả lời là không.
+Giới hạn ( lim ) trong định nghĩa tiệm cận ngang của hàm phân thức đã thể hiện rõ việc đths y=1/x chỉ tiến đến đt x=0 thôi chứ mãi mãi không thể chạm vào. Vì hiển nhiên kể cả khi x là rất lớn => 1/x là rất nhỏ, nhưng ta vẫn có thể lấy ra số 1/(x+1) là nhỏ hơn … Như vậy giữa đồ thị hàm số và tiện cận luôn có khoảng cách và mãi mãi không thể chạm vào ( hay cắt nhau )
+Giống như mình và cush vậy :(( đi mãi chỉ có thể làm bạn :((''',
+  'Sự cắt nhau không liên quan gì đến khái niệm tiệm cận nên chúng(tiệm cận và đths) có thể cắt nhau.',
+  'Vẫn cắt tốt',
+  'trưa mai có đá với Arsenal trên con đường chinh phục cúp C1 ko anh',
+  'có lắc',
+  'Nhớ west brom nhớ arsenal :((',
+  'Các vấn đề phát sinh, thắc mắc trong quá trình diễn ra sự kiện, các bạn vui lòng inbox',
+  'Nếu ngày mai bạn được lên công ty đi làm lại, bạn sẽ mặc gì nhỉ?',
+  'Để đảm bảo mục đích của thử thách, hình ảnh hoặc video clip phải có cùng 1 background nha',
+  'Các vấn đề phát sinh, thắc mắc trong quá trình diễn ra sự kiện, các bạn vui lòng inbox',
+  'Để đảm bảo mục đích của thử thách, hình ảnh hoặc video clip phải có cùng 1 background nha',
+  'Để đảm bảo mục đích của thử thách',
+  '''Italia dẫn đầu giải về chỉ số… phạm lỗi
+
+Với tổng cộng 93 lần bị thổi phạt vì phạm lỗi, Italia chính là đội tuyển phạm nhiều lỗi nhất tại EURO 2020. 
+
+Chưa hết, tập thể Italia đã 100 lần bị đối thủ phạm lỗi và cũng trở thành đội tuyển bị phạm lỗi nhiều nhất giải. 
+
+2 chỉ số này của ĐT Anh là 71 và 88. '''
+];
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<int?> showAlls = List.generate(comments.length, (index) => 3);
+
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //   body: Container(
-    //     margin: EdgeInsets.only(top: 200),
-    //     child: Comment(
-    //       avatar: ClipRRect(
-    //         borderRadius: BorderRadius.circular(32),
-    //         child: Image.network(
-    //           'https://scontent.fsgn4-1.fna.fbcdn.net/v/t1.6435-9/142432028_1736925749810194_5366117611380784464_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=ZLPTusW5vRQAX_A3jCs&_nc_ht=scontent.fsgn4-1.fna&oh=a7933121abb56f940a850425ddb35c6f&oe=613E5386',
-    //           width: 32,
-    //           height: 32,
-    //         ),
-    //       ),
-    //       shouldDrawBottomLine: true,
-    //       shouldDrawTopLine: true,
-    //       child: Container(
-    //         constraints: BoxConstraints(
-    //             maxWidth: MediaQuery.of(context).size.width - 76),
-    //         decoration: BoxDecoration(
-    //             color: Color(0xFFF5F6F7),
-    //             borderRadius: BorderRadius.circular(10)),
-    //         padding: EdgeInsets.all(16),
-    //         child: Text('''Các vấn đề phát sinh, sad fas asd
-    //             f asd
-    //              fas
-    //               f
-    //               as
-    //               fas
-    //                fas thắc mắc trong quá trình diễn ra sự kiện, các bạn vui lòng inbox'''),
-    //       ),
-    //     ),
-    //   ),
-    // );
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: ListView.builder(
-            itemCount: 20,
+            itemCount: comments.length,
             itemBuilder: (ctx, index) {
+              bool shouldDrawBottomLine =
+                  index == ((comments.length / 5).round() * 5)
+                      ? false
+                      : (index % 5 != 4) && index != comments.length - 1;
+              bool shouldDrawTopLine = index == 0 ? false : index % 5 != 1;
               return HighComment(
+                shouldDrawBottomLine: shouldDrawBottomLine,
+                shouldDrawTopLine: shouldDrawTopLine,
+                isParentComment: index % 5 == 0,
                 avatar: ClipRRect(
                   borderRadius: BorderRadius.circular(32),
                   child: Image.network(
@@ -57,29 +71,42 @@ class HomePage extends StatelessWidget {
                     height: 32,
                   ),
                 ),
-                // shouldDrawBottomLine: index == 19 ? false : true,
-                // shouldDrawTopLine: index == 0 ? false : true,
-                comment: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width - 76),
-                      decoration: BoxDecoration(
-                          color: Color(0xFFF5F6F7),
-                          borderRadius: BorderRadius.circular(10)),
-                      padding: EdgeInsets.all(16),
-                      child: Text('''Các vấn đề phát sinh, sad fas asd
-              f asd
-               fas
-                    f
-                    as 
-                    fas
-                     fas thắc mắc trong quá trình diễn ra sự kiện, các bạn vui lòng inbox'''),
+                actions: Container(
+                  height: 32,
+                  color: Colors.red,
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Text('like'),
+                      ),
+                      SizedBox(width: 8),
+                      Container(
+                        child: Text('reply'),
+                      ),
+                    ],
+                  ),
+                ),
+                comment: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      showAlls[index] = showAlls[index] == null ? 3 : null;
+                    });
+                  },
+                  behavior: HitTestBehavior.translucent,
+                  child: Container(
+                    constraints: BoxConstraints(
+                      maxWidth: MediaQuery.of(context).size.width - 76,
+                      minWidth: 140,
                     ),
-                    SizedBox(height: 46),
-                  ],
+                    decoration: BoxDecoration(
+                        color: Color(0xFFF5F6F7),
+                        borderRadius: BorderRadius.circular(10)),
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      comments[index],
+                      maxLines: showAlls[index],
+                    ),
+                  ),
                 ),
               );
             }),
